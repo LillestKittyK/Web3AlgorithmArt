67b59ca2158e99e13a9da2e79995bd322afb560b
alanvinton <128289642+LillestKittyK@users.noreply.github.com>
2023-03-19 15:49:05 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,78 +12,8 @@ let particles = [];
 let noiseFreq = 0.01;
 let noiseAmp = 200;
 
-// Global variable for smart contract instance
-let contractInstance;
-
-// Function to load smart contract data
-async function loadSmartContractData() {
-  const web3 = new Web3("https://ropsten.infura.io/v3/your-infura-project-id");
-  const contractAddress = "0x1234567890abcdef1234567890abcdef12345678";
-  const contractABI = [
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "_frequency",
-          "type": "uint256"
-        }
-      ],
-      "name": "setFrequency",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "name": "_amplitude",
-          "type": "uint256"
-        }
-      ],
-      "name": "setAmplitude",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "getFrequency",
-      "outputs": [
-        {
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "getAmplitude",
-      "outputs": [
-        {
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ];
-
-  contractInstance = new web3.eth.Contract(contractABI, contractAddress);
-
-  // Get the initial frequency and amplitude values from the smart contract
-  noiseFreq = await contractInstance.methods.getFrequency().call();
-  noiseAmp = await contractInstance.methods.getAmplitude().call();
-}
+// generate a random number between 0 and 0.1
+// let noiseFreq = Math.random() * 0.1;
 
 function setup() {
   // Create a canvas
@@ -117,7 +47,7 @@ function draw() {
     particles[i].draw();
   }
 
-  // Apply a blur effect to the entire canvas
+  // Apply a blur effect to the entire screen
   filter(BLUR, 10);
 }
 
@@ -159,7 +89,7 @@ class Particle {
         this.vel.limit(this.maxSpeed);
         this.pos.add(this.vel);
   
-        // Wrap the particle's position if it goes offscreen
+        // Make the particle's position appear to the other side if it goes offscreen
         if (this.pos.x < 0) {
           this.pos.x = width;
         }
@@ -199,12 +129,13 @@ class Particle {
     // Create a canvas
     createCanvas(windowWidth, windowHeight);
     colorMode(RGB);
-    //   set background colour to a light blue
 
-    background(random(255), random(255), random(255), 63);
+    //   set background colour to a random value
+    background(random(128,255), random(128,255), random(128,255), 63);
   
     // Initialize the flow field vectors
     for (let i = 0; i < cols; i++) {
+
       vectors[i] = [];
       for (let j = 0; j < rows; j++) {
         const angle = noise(i * 0.1, j * 0.1) * TWO_PI;
